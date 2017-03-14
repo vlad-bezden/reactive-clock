36971eac344b24a3c024584270567f0deac308d0
Vlad Bezden <vlad.bezden@gmail.com>
2017-03-13 22:11:51 -0400

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+render(<div>Reactive Clock</div>,
+    document.getElementById('root')
+)
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var webpack = require('webpack')
+
+module.exports = {
+    entry: "./src/index.jsx",
+    output: {
+        path: "dist",
+        filename: "bundle.js",
+        sourceMapFilename: 'bundle.map'
+    },
+    devtool: '#source-map',
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: ['babel-loader']
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader!autoprefixer-loader'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true,
+            warnings: false,
+            mangle: true
+        })
+    ]
+}
